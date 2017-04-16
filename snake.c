0b02c1442e2f51689a95a4aa2f83ac25b231922f
JeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2017-04-15 21:18:52 -0300

    .

---
+++ snake.c
@@ -0,0 +1,313 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <unistd.h>
+#include <conio.h>
+#define FOOD_CODE 4;
+#define SNAKE_CODE 5;
+
+//settings
+int gameOver = 0;
+
+//score
+int score = 0;
+
+//Stage
+int stage_linha = 30;
+int stage_coluna = 55;
+int original[30][55]={{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+					  {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2}};			  
+int game[30][55] = {};
+
+//Food
+struct sfood{
+	int x;
+	int y;
+};
+struct sfood food;
+int food_pos[2] = {};
+int foodEated = 0;
+int firstFood = 1;
+
+//Snake
+int snake[1650][2] = {{5, 20}, {5, 21}, {5, 22}};
+char direcao = 3;
+/* 0 = esquerda
+   1 = direita
+   2 = cima
+   3 = baixo
+*/
+//Functions declaration
+void reset_stage();
+void draw_stage();
+void clear_screen ( void );
+void addFood();
+void drawSnake();
+void moveSnake();
+void growSnake();
+int getSnakeSize();
+int randomInt(int n);
+char getObject(int n);
+void keyListener();
+void callGameOverScreen();
+
+int main(){
+	while(!gameOver){
+		clear_screen();
+		reset_stage();
+		moveSnake();
+		drawSnake();
+		addFood();
+		draw_stage();
+		Sleep(50);
+		keyListener();
+	}
+	callGameOverScreen();
+	return 0;
+}
+
+
+
+//auxiliar functions
+void clear_screen ( void )
+{
+  DWORD n;                         /* Number of characters written */
+  DWORD size;                      /* number of visible characters */
+  COORD coord = {0};               /* Top left screen position */
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+  /* Get a handle to the console */
+  HANDLE h = GetStdHandle ( STD_OUTPUT_HANDLE );
+
+  GetConsoleScreenBufferInfo ( h, &csbi );
+
+  /* Find the number of characters to overwrite */
+  size = csbi.dwSize.X * csbi.dwSize.Y;
+
+  /* Overwrite the screen buffer with whitespace */
+  FillConsoleOutputCharacter ( h, TEXT ( ' ' ), size, coord, &n );
+  GetConsoleScreenBufferInfo ( h, &csbi );
+  FillConsoleOutputAttribute ( h, csbi.wAttributes, size, coord, &n );
+
+  /* Reset the cursor to the top left position */
+  SetConsoleCursorPosition ( h, coord );
+}
+
+void reset_stage(){
+	int j, l;
+	
+	//Copia o stage original
+	for(l = 0; l < stage_linha; l++){
+		for(j = 0; j < stage_coluna; j++){
+			game[l][j] = original[l][j];
+		}
+	}
+}
+
+char getObject(int n){
+	char v = ' ';
+	
+	switch(n){
+		case 1:
+			v = '-';//parede horizontal
+			break;
+		case 2:
+			v = '+';//canto
+			break;
+		case 3:
+			v = '|';//parede vertical
+			break;
+		case 4:
+			v = '*'; //comidinha
+			break;
+		case 5:
+			v = 'o'; //cobrinha
+			break;
+		case 6:
+			v = 'x'; //crash
+			break;
+	}
+	
+	return v;
+}
+
+void draw_stage(){
+	int j, l;
+	
+	//draw score
+	printf("Score: %d\n", score);
+
+	//canvas
+	for(l = 0; l < stage_linha; l++){
+		for(j = 0; j < stage_coluna; j++){
+			int object_code = game[l][j];
+			char object = getObject(object_code);
+			printf("%c", object);
+		}
+		printf("\n");
+	}
+
+	//verifica se a cobrinha comeu a comidinha
+	if(snake[0][0] == food_pos[0] && snake[0][1] == food_pos[1]){
+		foodEated = 1;
+		growSnake();
+		addFood();
+		score++;
+	}
+}
+
+void addFood(){
+	//adiciona a comidinha em outro lugar
+	if(foodEated == 1 || firstFood == 1){
+		//enquanto a posição randomica da comidinha for invalida
+		do{
+			food.y = randomInt(stage_linha),
+			food.x = randomInt(stage_coluna);
+		}while(game[food.y][food.x] != 0);
+
+		//salva a posição atual da comidinha
+		food_pos[0] = food.y;
+		food_pos[1] = food.x;
+
+		game[food.y][food.x] = FOOD_CODE;
+
+		firstFood = 0;
+		foodEated = 0;
+	}else{
+		//senão a comidinha fica no mesmo lugar
+		game[food_pos[0]][food_pos[1]] = FOOD_CODE;
+	}
+}
+
+int randomInt(int n){
+	srand ( time(NULL) );
+	return rand() % n;
+}
+
+void drawSnake(){
+	int i, snakeSize = getSnakeSize();
+	for(i = 0; i < snakeSize; i++){
+		int snake_y = snake[i][0];
+		int snake_x = snake[i][1];
+		game[snake_y][snake_x] = SNAKE_CODE;
+	}
+}
+void moveSnake(){
+	int i, snakeSize = getSnakeSize();
+
+	for(i = snakeSize-1; i >= 1; i--){
+		snake[i][0] = snake[i-1][0];
+		snake[i][1] = snake[i-1][1];
+	}
+	if(direcao == 0){//esquerda
+		snake[0][1]--;
+	}else if(direcao == 1){//direita
+		snake[0][1]++;
+	}else if(direcao == 2){//cima
+		snake[0][0]--;
+	}else if(direcao == 3){//baixo
+		snake[0][0]++;
+	}
+
+	//se a cobrinha chegar no canto deve aparecer na outra extremidade
+
+	if(direcao == 3 && snake[0][0] == 30){//baixo
+		snake[0][0] = 0;
+	}else if(direcao == 2 && snake[0][0] == -1){//cima
+		snake[0][0] = 29;
+	}else if(direcao == 1 && snake[0][1] == 55){//direita
+		snake[0][1] = 0;
+	}else if(direcao == 0 && snake[0][1] == 0){//esquerda
+		snake[0][1] = 55;
+	}
+
+
+	//veirifica se a cobrinha se mordeu
+	int l;
+	for(l = 1; l < snakeSize; l++){
+		if(snake[0][0] == snake[l][0] && snake[0][1] == snake[l][1]){
+			gameOver = 1;
+		}
+	}
+
+}
+
+int getSnakeSize(){
+	int snakeMaxSize = (sizeof(snake)/sizeof(snake[0]));
+	int i;
+	
+	for(i = 0; i < snakeMaxSize; i++){
+		if(snake[i][0] == 0 && snake[i][1] == 0){
+			return i;
+		}
+	}
+	return i;
+}
+
+void growSnake(){
+	int snakeSize = getSnakeSize();
+	snake[snakeSize][0] = snake[snakeSize-1][0];
+	snake[snakeSize][1] = snake[snakeSize-1][1];
+}
+void keyListener(){
+
+	if ( _kbhit() ) {
+	    _getch();
+        switch(_getch()) { // the real value
+            case 75:
+            	if(direcao != 1)//direita
+                	direcao = 0;//esquerda
+                break;
+            case 77:
+            	if(direcao != 0)//esquerda
+                	direcao = 1;//direita
+                break;
+            case 72:
+            	if(direcao != 3)//baixo
+            	    direcao = 2;//cima
+                break;
+            case 80:
+            	if(direcao != 2)//cima
+            	    direcao = 3;//baixo
+                break;
+        }
+	}
+
+
+}
+
+void callGameOverScreen(){
+	clear_screen();
+	printf("Game over!\n");
+	game[snake[0][0]][snake[0][1]] = 6;
+	draw_stage();
+}
